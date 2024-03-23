9cf21a975216e8092b05b3543c96a17d863abe85
skpir4t3 <skg11786@gmail.com>
2024-03-23 14:09:56 +0530

    New Project + add feature added

---
+++ src/main/java/org/example/AddSub.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class AddSub {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(5,7));
+    }
+}
